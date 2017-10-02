aa734e36059ff038ee4ac28da7e3221ae4d50433
Michael Joyce <ubermichael@gmail.com>
2017-10-01 17:03:09 -0700

    get virus scanning done.

--- src/AppBundle/Command/Processing/DepositCommand.php
+++ src/AppBundle/Command/Processing/DepositCommand.php
@@ -66,7 +66,7 @@ class DepositCommand extends AbstractProcessingCmd
      */
     public function processingState()
     {
-        return 'reserialized';
+        return 'virus-checked';
     }
 
     /**
